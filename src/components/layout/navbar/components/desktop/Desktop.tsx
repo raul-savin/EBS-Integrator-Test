472d3e077b95a0f093876767ba90dedf84b9b8d6
webdevraul <webdevraul.github@gmail.com>
2024-12-03 13:50:53 +0000

    feat(navbar): create Desktop version component!

---
+++ src/components/layout/navbar/components/desktop/Desktop.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import Links from "../common/links/Links";
+
+const Desktop: FC = () => (
+  <ul className="hidden flex-row py-2 md:flex">
+    <Links />
+  </ul>
+);
+
+export default Desktop;
