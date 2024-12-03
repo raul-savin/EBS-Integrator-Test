085b20e931b325fca86e28d5094a4b6f20fe9236
webdevraul <webdevraul.github@gmail.com>
2024-12-03 14:04:57 +0000

    chore: add basic constants!

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const FAKE_API = "https://fakestoreapi.com/products?limit=12";
+export const SERVER_ERROR =
+  "Oops... Our servers are to bussy, please try again later.";
+
+export const REDIRECT_TO_HOME_PAGE = "/";
