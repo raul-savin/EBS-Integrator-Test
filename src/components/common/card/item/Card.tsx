afae7de8d4fd3ab4f33f6dfdf6c4deb4f24b7293
webdevraul <webdevraul.github@gmail.com>
2024-12-03 14:21:28 +0000

    feat: create reusable ItemCard component!

---
+++ src/components/common/card/item/Card.tsx
@@ -0,0 +1,53 @@
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Button from "./components/Button";
+
+import { Add, Remove } from "@/svg";
+import { useShoppingContext } from "@/context";
+import { Titem } from "@/types";
+
+const Card: FC<Titem> = ({ id, image, title, price, category }) => {
+  const { cart, addItemToCart, removeItemFromCart } = useShoppingContext();
+  const disableButtonIfNoQuantity = cart.map((item) => item.quantity)[0];
+  const navigate = useNavigate();
+
+  return (
+    <li className="four-column element-padding-tb">
+      <article className="flex flex-col justify-center items-center shadow-md element-padding-lr  element-padding-tb !pt-0">
+        <figure>
+          <figcaption className="flex justify-center element-padding-tb !pt-0 capitalize text-xs">
+            {category}
+          </figcaption>
+          <img
+            src={image}
+            alt={title}
+            onClick={() => navigate(`/product/${id}`)}
+            className="hover:cursor-pointer"
+          />
+        </figure>
+        <header className="element-padding-tb text-center">
+          <h3>{title}</h3>
+        </header>
+        <footer className="flex justify-between w-full h-[40px]">
+          <Button
+            onClick={() => removeItemFromCart({ id })}
+            disabled={!disableButtonIfNoQuantity}
+            label="remove product from cart"
+          >
+            <Remove />
+          </Button>
+          <span className="flex items-center font-medium">{price}$</span>
+          <Button
+            onClick={() => addItemToCart({ id })}
+            label="add product to cart"
+          >
+            <Add />
+          </Button>
+        </footer>
+      </article>
+    </li>
+  );
+};
+
+export default Card;
