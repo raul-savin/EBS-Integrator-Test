0ccdd161d929b0bec0d8668fdb025a7f2b366fbc
webdevraul <webdevraul.github@gmail.com>
2024-12-03 14:25:20 +0000

    chore: add new props for TshoppingCartContextType!

--- src/types/state-management/index.tsx
+++ src/types/state-management/index.tsx
@@ -1,6 +1,10 @@
 import { Titem } from "../client";
 
 export type TshoppingCartContextType = {
+  cart: Titem[];
   data: Titem[];
   updateData: (data: Titem[]) => void;
+  addItemToCart: ({ id }: Pick<Titem, "id">) => void;
+  removeItemFromCart: ({ id }: Pick<Titem, "id">) => void;
+  clearCart: () => void;
 };
