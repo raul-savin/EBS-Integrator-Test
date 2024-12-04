7283ec5a2c560a44af1c546aacb06d7faabd3be3
webdevraul <webdevraul.github@gmail.com>
2024-12-04 07:59:25 +0000

    feat: create ShoppinBag component to show how many items are in Cart for Home page!

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,2 +1,3 @@
 export { default as Filter } from "./filter/Filter";
+export { default as ShoppingBag } from "./shoppingBag/ShoppingBag";
 export { default as Sort } from "./sort/Sort";
---
+++ src/components/pages/home/shoppingBag/ShoppingBag.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+
+import { Cart } from "@/svg";
+import { Titem } from "@/types";
+import { useNavigate } from "react-router-dom";
+
+const ShoppingBag: FC<{ cart: Titem[] }> = ({ cart }) => {
+  const navigate = useNavigate();
+
+  const items = cart.reduce((accumulator, current) => {
+    return accumulator + current.quantity;
+  }, 0);
+
+  return (
+    <aside
+      className="fixed right-[1.25rem] hover:cursor-pointer md:right-6 md:top-10 lg:top-8 lg:right-8 top-16 z-10"
+      onClick={() => navigate("/cart")}
+    >
+      <figure className="flex justify-center items-center">
+        <Cart elClass="fill-[#fff]" />
+        <span className="absolute text-xs font-medium text-blue mt-[12px]">
+          {items}
+        </span>
+      </figure>
+    </aside>
+  );
+};
+
+export default ShoppingBag;
