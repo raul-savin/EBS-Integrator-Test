c997e20f5b792a7d9e8719373b95e71d997e3786
webdevraul <webdevraul.github@gmail.com>
2024-12-04 03:34:17 +0000

    feat: add ClearCart method to Cart Footer component!

--- src/components/pages/cart/footer/Footer.tsx
+++ src/components/pages/cart/footer/Footer.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
 
-const Footer: FC = () => (
+const Footer: FC<{ clearCart: () => void }> = ({ clearCart }) => (
   <footer className="w-full flex justify-center items-center section-padding-tb">
-    <button className="bg-red text-white py-2 px-6 capitalize" type="button">
+    <button
+      className="bg-red text-white py-2 px-6 capitalize"
+      type="button"
+      onClick={() => clearCart()}
+    >
       remove all items
     </button>
   </footer>
