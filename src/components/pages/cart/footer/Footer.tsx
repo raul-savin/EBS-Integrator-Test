05611e282937a0b9c853257ca40a7b196b5627d4
webdevraul <webdevraul.github@gmail.com>
2024-12-04 03:24:24 +0000

    feat: add Cart Footer component!

---
+++ src/components/pages/cart/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Footer: FC = () => (
+  <footer className="w-full flex justify-center items-center section-padding-tb">
+    <button className="bg-red text-white py-2 px-6 capitalize" type="button">
+      remove all items
+    </button>
+  </footer>
+);
+
+export default Footer;
