782cf805c3f5120a3d72b0252e63e6a37f3d748b
webdevraul <webdevraul.github@gmail.com>
2024-12-04 13:51:46 +0000

    style: decrease size for ItemCard Button component for mobile version!

--- src/components/common/card/item/components/Button.tsx
+++ src/components/common/card/item/components/Button.tsx
@@ -12,7 +12,7 @@ const Button: FC<TcardButtton> = ({ label, children, onClick, disabled }) => (
   <button
     type="button"
     className={cn(
-      "border-[1.5px] border-blue flex justify-center hover:bg-blue-light items-center size-[40px] [&>svg]:text-blue",
+      "border-[1.5px] border-blue flex justify-center hover:bg-blue-light items-center size-[30px] lg:size-[40px] [&>svg]:text-blue",
       { "hover:bg-white": disabled }
     )}
     aria-label={label}
